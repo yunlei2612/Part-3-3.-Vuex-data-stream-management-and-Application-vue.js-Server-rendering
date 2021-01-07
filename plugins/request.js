b40809001000ef894c6597d3a03a8252c299d5df
yunlei2612* <13616128742@163.com>
2021-01-07 18:42:27 +0800

    发布部署

--- plugins/request.js
+++ plugins/request.js
@@ -1,14 +1,11 @@
 /**
  * 基于 axios 封装的请求模块
  */
-
 import axios from 'axios'
-
 // 创建请求对象
 export const request = axios.create({
   baseURL: 'http://realworld.api.fed.lagounews.com'
 })
-
 // 通过插件机制获取到上下文对象（query、params、req、res、app、store...）
 // 插件导出函数必须作为 default 成员  
 export default ({ store }) => {
